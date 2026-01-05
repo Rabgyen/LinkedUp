0eb5f968c1a455978ec38a750f76dac1d7ad4833
Rabgyen <rabmoktan13@gmail.com>
2026-01-05 19:25:37 +0545

    first commit

---
+++ users/components/navbar.php
@@ -0,0 +1,33 @@
+<navbar style="height: 100%; width: 100%">
+    <div class="nav-items">
+        <div class="logo">
+            <img src="../../images/linkedup-logo.png" alt="logo">
+        </div>
+        <div class="links-container">
+            <input type="checkbox" id="sidebar-activate">
+            <label for="sidebar-activate" class="sidebar-open">
+                <i class="fa-solid fa-bars"></i>
+            </label>
+            <div class="page-links">
+                <label for="sidebar-activate" class="sidebar-close">
+                    <i class="fa-solid fa-x"></i>
+                </label>
+                <a href="#"><i class="fa-solid fa-house"></i></a>
+                <a href="#"><i class="fa-solid fa-suitcase"></i></a>
+                <a href="#"><i class="fa-solid fa-bell"></i></a>
+                <a href="#" id="user"><i class="fa-solid fa-user"></i></a>
+            </div>
+            <div class="search-container-wrapper">
+                <div class="search-container">
+                    <form action="" method="post">
+                        <input type="text" name="search" placeholder="Search">
+                    </form>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <div class="profile">
+                    <img src="../../images/user.jpg" alt="profile image">
+                </div>
+            </div>
+        </div>
+    </div>
+</navbar>
---
+++ users/components/profileCard.php
@@ -0,0 +1,22 @@
+<div class="profile-card-container">
+    <div class="profile-card">
+        <div class="profile-info">
+            <div class="profile-img">
+                <img src="../../images/user.jpg" alt="profile-img">
+            </div>
+            <span>
+                <h2>User name</h2>
+                <p>Software Developer</p>
+                <p><i class="fa-solid fa-location-dot"></i> Kathmandu, Nepal</p>
+                <p>Software Engineer</p>
+                <a href="#"><p>Manage Profile</p><i class="fa-solid fa-arrow-right"></i></a>
+            </span>
+        </div>
+    </div>
+    <div class="menu-container">
+        <a href="#"><i class="fa-solid fa-bookmark"></i>  Saved items</a>
+        <a href="#"><i class="fa-solid fa-comment"></i>  Comments</a>
+        <a href="#"><i class="fa-solid fa-calendar"></i>  Events</a>
+        <a href="#"><i class="fa-solid fa-newspaper"></i>  Newsletters</a>
+    </div>
+</div>
